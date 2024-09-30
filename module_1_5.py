4adda72c606c7827df0670b848c7f4c2c55e08a8
Klyueva Ekaterina <Kuska2006@list.ru>
2024-10-01 01:59:32 +0300

    Неизменяемые и изменяемые объекты. Кортежи

--- module_1_5.py
+++ module_1_5.py
@@ -2,7 +2,7 @@ immutable_var=(1,2,3,True,"Urban")
 print(immutable_var)
 #immutable_var[0]=2
 #print(immutable_var) # Кортеж не изменяемый объект, если отобразить так выводит ошибку
-mutable_list=(1,2,3,True,'Urban')
+mutable_list=('work','sleep','paty')
 print(mutable_list)
-immutable_list[0]=2
+mutable_list[1]='Vacation'
 print(mutable_list)
